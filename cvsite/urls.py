b268d8f6e161a1c6e5c31b9813598ae3a5de1163
laneb <lane.barlow@gmail.com>
2016-11-24 19:56:47 -0800

    create skeleton route

--- cvsite/urls.py
+++ cvsite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+	url(r'^$', views.index, name='index'),
+    url(r'^mathematics/', include('mathematics.urls')),
+    url(r'^programming/', include('programming.urls')),
+    url(r'^photography/', include('photography.urls')),
     url(r'^admin/', admin.site.urls),
 ]
