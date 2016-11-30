2edf88bb656216d9cd985c626ffb00c385a7df1d
laneb <lane.barlow@gmail.com>
2016-11-30 01:27:04 -0800

    add actual photography projects

--- photography/views.py
+++ photography/views.py
@@ -5,45 +5,89 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-  template = loader.get_template('cvsite/proj-index.html')
-  context = {
-    'page_name': "photography",
-    'projects': [
-      {
-        'title': "Power Lines: Land Scraped",
-        'url': "powerlines-landscraped",
-        'desc': "pwrln mfn lndscps"
-      },
+import re
 
+projects_by_path = {
+  'powerlines-sketches': {
+    'title': "Power Lines: Sketches",
+    'desc': "pwrln mfn lndscps",
+    'url': 'powerlines-sketches',
+    'images': [
       {
-        'title': "Power Lines: Sketches",
-        'url': "powerlines-sketches",
-        'desc': "sum dang pwrln slhtts"
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_2507.jpg",
+        'title': "untitled (1)",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4052.jpg",
+        'title': "untitled (2)",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_2519.jpg",
+        'title': "torn",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4559.jpg",
+        'title': "untitled (3)",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4237.jpg",
+        'title': "untitled (4)",
+        'desc': 'inkjet on paper'
       }
     ]
+  },
+
+  'powerlines-landscraped': {
+    'title': "Power Lines: Land, Scraped",
+    'desc': "sum pwr lns 2",
+    'url': "powerlines-landscraped",
+    'images': [
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4785.jpg",
+        'title': "morning on the ridge",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4957.jpg",
+        'title': "recent developments",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_3916.jpg",
+        'title': "obelisk",
+        'desc': 'inkjet on paper'
+      },
+      {
+        'url': "http://lanebarlow.s3.amazonaws.com/_MG_4725.jpg",
+        'title': "reprieve",
+        'desc': 'inkjet on paper'
+      }
+    ]
+  }
+}
+
+def index(request):
+  template = loader.get_template('cvsite/proj-index.html')
+
+  context = {
+    'page_name': "photography",
+    'projects': projects_by_path.values
   }
   return HttpResponse(template.render(context, request))
 
 def project(request):
-  template = loader.get_template('photography/project.html')
+  template = loader.get_template('photography/photo-project.html')
+
+  project_path = re.search('/photography/(.*)', request.path).group(1)
+  project = projects_by_path[project_path]
+
   context = {
-    'project_name': 'Power Lines',
-    'images': [
-      {
-        'url': "http://lanebarlow.s3.amazonaws.com/_MG_0998.jpg",
-        'title': "pwrln1",
-        'desc': 'archival inkjet on canvas'
-      },
-      {
-        'url': "http://lanebarlow.s3.amazonaws.com/_MG_1001.jpg",
-        'title': "pwrln2"
-      },
-      {
-        'url': "http://lanebarlow.s3.amazonaws.com/_MG_2154.jpg",
-        'title': "pwrln3"
-      }
-    ],
+    'project_name': project['title'],
+    'images': project['images'],
     'photo_height': 624,
     'photo_width': 926
   }
